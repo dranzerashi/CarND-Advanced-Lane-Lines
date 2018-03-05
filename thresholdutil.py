59f18e436d91405f2af9c114a44fc84b5e0e9ac5
Ashith Raghunath <ashith.raghunath@ad.infosys.com>
2018-03-05 22:50:05 +0530

    Thresholding sobel

---
+++ thresholdutil.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+
+def scale_sobel(img):
+    return np.uint8((img*255)/np.max(img))
+
+
+def binary_threshold(img, thresh=(0,255)):
+    binary_output = np.zeros_like(img)
+    binary_output[(img > thresh[0]) & (img <= thresh[1])] = 1
+    return binary_output
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Calculate directional gradient
+    # Apply threshold
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sx, sy = (1, 0) if orient == 'x' else (0, 1)
+    sobel = cv2.Sobel(gray, cv2.CV_64F, sx, sy, ksize=sobel_kernel)
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = scale_sobel(abs_sobel)
+    grad_binary = binary_threshold(scaled_sobel, thresh)
+    return grad_binary
+
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    # Apply threshold
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobel = np.sqrt(np.square(sobelx) + np.square(sobely))
+    scaled_sobel = scale_sobel(abs_sobel)
+    mag_binary = binary_threshold(scaled_sobel, mag_thresh)
+    return mag_binary
+
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    # Apply threshold
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobel_x = np.sqrt(np.square(sobelx))
+    abs_sobel_y= np.sqrt(np.square(sobely))
+    grad_dir = np.arctan2(abs_sobel_y, abs_sobel_x)
+    dir_binary = binary_threshold(grad_dir, thresh)
+    return dir_binary
